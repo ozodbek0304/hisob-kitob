b8e982d822ea0f533ab8204c4567cb51a2c4f51d
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-03-12 12:44:03 +0500

    ignore

--- src/lib/query-client.tsx
+++ src/lib/query-client.tsx
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
-            retry: import.meta.env.MODE === "development" ? 0 : 3,
+            retry: process.env.NODE_ENV === "development" ? 0 : 3,
             gcTime: 1000 * 60 * 2,
         },
     },
